827c0f27b0ebc0f5814ee663295a932abb80cf5d
andrewmck95 <39978143+andrewmck95@users.noreply.github.com>
2018-06-21 04:24:02 +0100

    Add files via upload

---
+++ Primitive/Big_O_Notation/Linear_Growth.java
@@ -0,0 +1,48 @@
+
+public class Linear_Growth {
+
+	public static int n = 0;
+
+	public static int sum(int[] arr) {
+		int total = 0;
+		for (int i = 0; i < arr.length; i++) {
+			total += arr[i];
+		}
+		return total;
+	}
+
+	public static long timing(int[] arr) {
+		// calling sum function
+		long startTime = System.nanoTime();
+		int x = sum(arr);
+		long stopTime = System.nanoTime();
+		long runtime = stopTime - startTime;
+		System.out.println("Sum of array is: " + x);
+		System.out.println("Runtime for arraysize " + arr.length + " is: " + runtime + " nanoseconds");
+		return runtime;
+	}
+
+	public static void populateArr(int[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = i + 1;
+		}
+	}
+
+	public static void run(int[] arr) {
+		populateArr(arr);
+		timing(arr);
+		System.out.println("");
+	}
+
+	public static void main(String args[]) {
+		int[] nums1 = new int[10];
+		int[] nums2 = new int[1000];
+		int[] nums3 = new int[10000];
+		int[] nums4 = new int[100000];
+		run(nums1);
+		run(nums2);
+		run(nums3);
+		run(nums4);
+
+	}
+}
